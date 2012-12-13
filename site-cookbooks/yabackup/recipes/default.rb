4be37eecee2de8fd16d859bb1edebdf9d46d3f7f
Andrey Subbota <subbota@gmail.com>
2012-12-13 10:34:36 +0400

    Minor update yabackup

--- site-cookbooks/yabackup/recipes/default.rb
+++ site-cookbooks/yabackup/recipes/default.rb
@@ -4,25 +4,26 @@
 #
 package "davfs2"
 
-directory "#{node['yabackup']['mountdir']}" do
-  owner "#{node['yabackup']['owner_user']}"
-  group "#{node['yabackup']['owner_group']}"
+directory node['yabackup']['mountdir'] do
+  owner node['yabackup']['owner_user']
+  group node['yabackup']['owner_group']
   mode "0755"
   action :create
   recursive true
 end
 
-yasecret = data_bag_item('secrets', "#{node['yabackup']['secrets']}")
+yasecret = data_bag_item('secrets', node['yabackup']['secrets'])
 
 execute "check_yadisk_pass" do
-  user "#{node['yabackup']['owner_user']}"
+  user node['yabackup']['owner_user']
   command "echo '#{node['yabackup']['webdav_url']} #{yasecret['username']} #{yasecret['password']}' >> /etc/davfs2/secrets"
   not_if "grep -q '#{node['yabackup']['webdav_url']}' /etc/davfs2/secrets"
 end
 
-mount "/mnt/yabackup/" do
+mount node['yabackup']['mountdir'] do
   fstype "davfs"
-  device "#{node['yabackup']['webdav_url']}"
-  options "rw"
+  device node['yabackup']['webdav_url']
+  options "rw,_netdev,users,exec"
   action [:mount, :enable]
+  not_if "mountpoint -q #{node['yabackup']['mountdir']}"
 end
