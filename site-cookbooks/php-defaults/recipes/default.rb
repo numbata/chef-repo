4dd9346796cbac3a0727f198e08576eb72aae460
Andrey Subbota <subbota@gmail.com>
2013-09-17 11:45:41 +0400

    Added php-fpm config

--- site-cookbooks/php-defaults/recipes/default.rb
+++ site-cookbooks/php-defaults/recipes/default.rb
@@ -4,3 +4,16 @@ template "/etc/php5/conf.d/timezone.ini" do
   group "root"
   mode "0644"
 end
+
+template "/usr/local/etc/php-fpm.conf" do
+  source "php-fpm.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+directory "/var/www/fpm.d/" do
+  owner "www-data"
+  group "www-data"
+  action :create
+end
